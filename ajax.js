138fad56deb2877e441b7a50621b669b5a64708b
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-23 12:55:51 -0500

    completed step 3

--- ajax.js
+++ ajax.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
+  var button = document.body.querySelector('#button')
+  button.addEventListener('click', function(e){
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/',
       method:'GET',
       dataType:'text',
     });
-
   });
-  /* Your code goes here */
+});
